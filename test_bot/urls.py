6395c791ee33f528a740b905d1e102659c7b8e6f
hane <hane0132@gmail.com>
2020-12-18 10:54:48 +0800

    set url for chat

--- test_bot/urls.py
+++ test_bot/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from chat.views import chat
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat/<slug:session_id>/', chat),
 ]
