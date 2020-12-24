9bd9e25d804b1bca0ea88a2965124e05ecf0b785
hane <hane0131@gmail.com>
2020-12-24 23:55:09 +0800

    test

--- chat/views.py
+++ chat/views.py
@@ -75,9 +75,8 @@ def chat(request):
             language_code
         )
         for msg in response.query_result.fulfillment_messages:
-            print(dir(msg))
-            print(dir(msg._pb))
-            if not hasattr(msg._pb, 'platform'):
+            print(msg.platform)
+            if msg.platform == 'default':
                 msg_list.append('Bot: '+msg.text.text[0])
         # request.session[session_id] = msg_list
         with open('chat_log.json', 'w') as f:
